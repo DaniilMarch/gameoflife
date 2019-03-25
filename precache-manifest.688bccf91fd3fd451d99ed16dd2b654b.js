d6241a68c75c3a5633820524d2fffc4fc61badba
DaniilMarch <asmandian@gmail.com>
2019-03-25 10:29:45 +0300

    Initial

---
+++ precache-manifest.688bccf91fd3fd451d99ed16dd2b654b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gameoflife/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "134693c1149dfffb8647",
+    "url": "/gameoflife/static/js/runtime~main.72a08090.js"
+  },
+  {
+    "revision": "af9553d416e2efc5d111",
+    "url": "/gameoflife/static/js/main.8ca86e1d.chunk.js"
+  },
+  {
+    "revision": "56eb1871b148b8c61f80",
+    "url": "/gameoflife/static/js/2.22a2f82f.chunk.js"
+  },
+  {
+    "revision": "af9553d416e2efc5d111",
+    "url": "/gameoflife/static/css/main.b258a538.chunk.css"
+  },
+  {
+    "revision": "505b604ca2924ac629541e71e46f3eca",
+    "url": "/gameoflife/index.html"
+  }
+];
